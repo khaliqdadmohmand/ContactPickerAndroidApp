8b5df7ce2572e7debc23c002bcedcfd2127e447c
khaliq.bestweb <khaliq.bestweb@gmail.com>
2019-05-19 01:53:59 +0400

    Initial commit

---
+++ app/src/main/java/www/eemobilemoney/selectcontact/MainActivity.java
@@ -0,0 +1,74 @@
+package www.eemobilemoney.selectcontact;
+
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.provider.ContactsContract;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int RESULT_PICK_CONTACT =1;
+    private TextView phone;
+    private Button select;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate (savedInstanceState);
+        setContentView (R.layout.activity_main);
+
+        phone = findViewById (R.id.phone);
+        select = findViewById (R.id.select);
+
+        select.setOnClickListener (new View.OnClickListener () {
+            @Override
+            public void onClick(View v) {
+                Intent in = new Intent (Intent.ACTION_PICK, ContactsContract.CommonDataKinds.Phone.CONTENT_URI);
+                startActivityForResult (in, RESULT_PICK_CONTACT);
+            }
+        });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode,  Intent data) {
+        if(resultCode==RESULT_OK)
+        {
+            switch (requestCode) {
+                case RESULT_PICK_CONTACT:
+                    contactPicked (data);
+                    break;
+            }
+        }
+        else
+        {
+            Toast.makeText (this, "Failed To pick contact", Toast.LENGTH_SHORT).show ();
+        }
+    }
+
+    private void contactPicked(Intent data) {
+        Cursor cursor = null;
+
+        try {
+            String phoneNo = null;
+            Uri uri = data.getData ();
+            cursor = getContentResolver ().query (uri, null, null,null,null);
+            cursor.moveToFirst ();
+            int phoneIndex = cursor.getColumnIndex (ContactsContract.CommonDataKinds.Phone.NUMBER);
+
+            phoneNo = cursor.getString (phoneIndex);
+
+            phone.setText (phoneNo);
+
+
+        } catch (Exception e) {
+            e.printStackTrace ();
+        }
+    }
+}
